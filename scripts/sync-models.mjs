bbb63879b81fa593a1911dbfff8df4eaa55d8ab6
fpissaip-source <fpissa.ip@gmail.com>
2026-06-25 09:56:55 +0200

    Remove unused encrypted model from build sync

--- scripts/sync-models.mjs
+++ scripts/sync-models.mjs
@@ -13,11 +13,6 @@ const assets = [
     sha256: "c287a4d10bbbbc4c779f7b0a68b666b530ef9754142843aa048b4d5976c0d052",
   },
   {
-    name: "character.enc",
-    url: "https://media.githubusercontent.com/media/MoncyDev/Portfolio-Website/main/public/models/character.enc",
-    sha256: "727391097c9c122211f7e3f5e18f219765f15450abb7f49be15b5703500476b7",
-  },
-  {
     name: "char_enviorment.hdr",
     url: "https://media.githubusercontent.com/media/MoncyDev/Portfolio-Website/main/public/models/char_enviorment.hdr",
     sha256: "366401083df3216cf9dd21d113d1d02c5f55d474ee9ffa121af1486a5396eccf",
