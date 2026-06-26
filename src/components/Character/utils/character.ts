d09104f273b074ebf3eaa8119f5b80b320b8c5ad
fpissaip-source <fpissa.ip@gmail.com>
2026-06-27 01:41:10 +0200

    Use encrypted GLB character loader

--- src/components/Character/utils/character.ts
+++ src/components/Character/utils/character.ts
@@ -1,26 +1,59 @@
 import * as THREE from "three";
-import type { GLTF } from "three-stdlib";
-import createIssaCharacter from "./createIssaCharacter";
+import { DRACOLoader, GLTF, GLTFLoader } from "three-stdlib";
+import { setCharTimeline, setAllTimeline } from "../../utils/GsapScroll";
+import { decryptFile } from "./decrypt";
 
 const setCharacter = (
   renderer: THREE.WebGLRenderer,
   scene: THREE.Scene,
   camera: THREE.PerspectiveCamera
 ) => {
-  const loadCharacter = async (): Promise<GLTF> => {
-    const gltf = createIssaCharacter();
-    const character = gltf.scene;
+  const loader = new GLTFLoader();
+  const dracoLoader = new DRACOLoader();
+  dracoLoader.setDecoderPath("/draco/");
+  loader.setDRACOLoader(dracoLoader);
 
-    character.traverse((child) => {
-      if (child instanceof THREE.Mesh) {
-        child.castShadow = true;
-        child.receiveShadow = true;
-        child.frustumCulled = true;
+  const loadCharacter = () => {
+    return new Promise<GLTF | null>(async (resolve, reject) => {
+      try {
+        const encryptedBlob = await decryptFile(
+          "/models/character.enc",
+          "Character3D#@"
+        );
+        const blobUrl = URL.createObjectURL(new Blob([encryptedBlob]));
+
+        let character: THREE.Object3D;
+        loader.load(
+          blobUrl,
+          async (gltf) => {
+            character = gltf.scene;
+            await renderer.compileAsync(character, camera, scene);
+            character.traverse((child: any) => {
+              if (child.isMesh) {
+                const mesh = child as THREE.Mesh;
+                child.castShadow = true;
+                child.receiveShadow = true;
+                mesh.frustumCulled = true;
+              }
+            });
+            resolve(gltf);
+            setCharTimeline(character, camera);
+            setAllTimeline();
+            character!.getObjectByName("footR")!.position.y = 3.36;
+            character!.getObjectByName("footL")!.position.y = 3.36;
+            dracoLoader.dispose();
+          },
+          undefined,
+          (error) => {
+            console.error("Error loading GLTF model:", error);
+            reject(error);
+          }
+        );
+      } catch (err) {
+        reject(err);
+        console.error(err);
       }
     });
-
-    await renderer.compileAsync(character, camera, scene);
-    return gltf;
   };
 
   return { loadCharacter };
