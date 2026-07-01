eea7b8f13ebec89c5766701c8048ad8f93b5d7e9
fpissaip-source <fpissa.ip@gmail.com>
2026-07-01 11:22:22 +0200

    Create complete application root

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Landing from "./components/Landing";
+import About from "./components/About";
+import ProjectList from "./components/ProjectList";
+import Contact from "./components/Contact";
+
+export default function App() {
+  return (
+    <main>
+      <Landing />
+      <About />
+      <ProjectList />
+      <Contact />
+    </main>
+  );
+}
